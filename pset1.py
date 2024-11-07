1cd5b647e00ee810552083da345727b7d306d76f
Antonio Rafael <103446212+rafasam123@users.noreply.github.com>
2024-11-07 15:54:32 -0300

    Update pset1.py

--- pset1.py
+++ pset1.py
@@ -41,18 +41,31 @@ class Imagem:
         self.pixels = pixels
 
     def get_pixel(self, x, y):   
-        def limitar(valor, minimo, maximo): #função auxilair para deixar modular
+        """
+        Retorna o valor do pixel na posição (x, y) da imagem.
+        Caso as coordenadas fornecidas estejam fora dos limites da imagem,
+        utiliza o valor mais próximo dentro dos limites.
+        """
+        def limitar(valor, minimo, maximo): #função auxilair
             return max(minimo, min(valor, maximo))
         
         x = limitar(x, 0, self.largura - 1)
         y = limitar(y, 0, self.altura - 1)
 
         return self.pixels[y * self.largura + x]   #Tinha uma tupla(corrigido). Retorna o pixel na posição (x, y) da imagem.
+        
 
-    def set_pixel(self, x, y, c):             
+    def set_pixel(self, x, y, c): 
+        """
+        Define o valor do pixel na posição (x, y) da imagem com a cor (intensidade) fornecida.
+        """            
         self.pixels[y * self.largura + x] = c #Tinha uma tupla(corrigido). Define o pixel na posição (x, y) da imagem com a cor c
 
     def aplicar_por_pixel(self, func):
+        """
+        Aplica uma função a cada pixel da imagem, criando uma nova imagem resultante.
+        Retorna a imagem resultante após a aplicação da função a cada pixel.
+        """
         resultado = Imagem.nova(self.largura, self.altura)
         for x in range(resultado.largura):
             for y in range(resultado.altura):
@@ -62,20 +75,32 @@ class Imagem:
         return resultado
     
     def correlacao(self, kernel):
+        """
+        Aplica uma operação de correlação na imagem usando o kernel fornecido.
+        Cada pixel da imagem resultante é calculado com base nos valores de intensidade
+        dos pixels vizinhos na imagem original e nos pesos definidos pelo kernel.
+        Retorna uma nova imagem com o resultado da correlação.
+        """
         tamanho_kernel = len(kernel)
         delocamento = tamanho_kernel // 2
         imagem_resultado = Imagem.nova(self.largura, self.altura)
         for x in range(self.largura):
             for y in range(self.altura):
                 soma_correlacao = 0
+                # Percorre o kernel e soma os produtos dos pixels adjacentes
                 for w in range(tamanho_kernel):
                     for z in range(tamanho_kernel):
-                        pixel = self.get_pixel(x - delocamento + w, y - delocamento + z)
-                        soma_correlacao += pixel * kernel[w][z]
-                imagem_resultado.set_pixel(x, y, soma_correlacao)
+                        pixel = self.get_pixel(x - delocamento + w, y - delocamento + z) # Obtém o pixel adjacente
+                        soma_correlacao += pixel * kernel[w][z] # Multiplica pelo valor do kernel
+                imagem_resultado.set_pixel(x, y, soma_correlacao) # Armazena o resultado na nova imagem
         return imagem_resultado
     
     def pixel_normalizado(self):
+        """
+        Normaliza os valores dos pixels da imagem, limitando-os entre 0 e 255.
+        Esta função é útil após operações como correlação, onde os valores dos
+        pixels podem exceder esses limites.
+        """
         def normalizar_valor(valor):
             return max(0, min(255, round(valor)))
         for x in range(self.largura):
@@ -87,38 +112,58 @@ class Imagem:
         
 
     def invertida(self):                                 #Estava 256(corrigido) 
+        """
+        Cria uma nova imagem onde cada pixel é a inversão do pixel correspondente
+        na imagem original (255 - valor do pixel).
+        """
         return self.aplicar_por_pixel(lambda c: 255 - c) # Aplica uma função que inverte a cor de cada pixel e retorna o resultado
 
     def borrada(self, n):
-        kernel = self.correlacao(montar_kernel(n))
-        kernel.pixel_normalizado()
+        """
+        Aplica um efeito de desfoque na imagem com base em um kernel de média de tamanho n.
+        Retorna uma nova imagem com o efeito de desfoque.
+        """
+        kernel = self.correlacao(montar_kernel(n))# Aplica a correlação com o kernel
+        kernel.pixel_normalizado() # Normaliza os pixels da nova imagem borrada
         return kernel
 
     def focada(self, n):
-        imagemborrada = self.borrada(n)
+        """
+        Aplica um efeito de foco na imagem ao subtrair uma imagem desfocada (obtida com o kernel de média de tamanho n)
+        da imagem original. Normaliza os valores de pixel da imagem focada.
+        """
+        imagemborrada = self.borrada(n) # Aplica o desfoque
         imagemfocada = Imagem.nova(self.largura, self.altura)
         for x in range(self.largura):
             for y in range(self.altura):
                 valor_pixel_original = self.get_pixel(x, y)
+                # Calcula a diferença entre os pixels da imagem original e a imagem borrada
                 valor_pixel_focada = round(2 * valor_pixel_original - imagemborrada.get_pixel(x,y))
                 imagemfocada.set_pixel(x, y, valor_pixel_focada)
         imagemfocada.pixel_normalizado()
         return imagemfocada
 
     def bordas(self):
+        """
+        Detecta as bordas da imagem usando o operador Sobel. Calcula a magnitude do gradiente
+        para cada pixel, aplicando as matrizes de kernel Sobel nos eixos X e Y.
+        Retorna uma nova imagem com as bordas detectadas.
+        """
         imagemborda = Imagem.nova(self.largura, self.altura)
+        # Detecta bordas usando o operador de Sobel.
         KernelX =   [[-1, 0, 1],
                     [-2, 0, 2],
                     [-1, 0, 1]]
         KernelY =   [[-1, -2, -1],
                     [0, 0, 0],
                     [1, 2, 1]]
-        SobelXaplicar = self.correlacao(KernelX)
-        SobelYaplicar = self.correlacao(KernelY)
+        SobelXaplicar = self.correlacao(KernelX) # Aplica o kernel para o eixo X
+        SobelYaplicar = self.correlacao(KernelY) # Aplica o kernel para o eixo Y
         for x in range(self.largura):
             for y in range(self.altura):
                 valor_sobel_X = SobelXaplicar.get_pixel(x, y)
                 valor_sobel_Y = SobelYaplicar.get_pixel(x, y)
+                # Calcula a magnitude do gradiente utilizando a raiz quadrada da soma dos quadrados
                 sobelOperation = round(math.sqrt(valor_sobel_X ** 2 + valor_sobel_Y ** 2))
                 imagemborda.set_pixel(x, y, sobelOperation)
         imagemborda.pixel_normalizado()
@@ -319,6 +364,9 @@ if __name__ == '__main__':
     Imagem.salvar(construcaosobelY, 'resultadosimg/construcaosobelYQuestão6.png')
     construcaoborda = construcao.bordas()
     Imagem.salvar(construcaoborda, 'resultadosimg/construcaobordaQuestão6.png')
+
+    imagem_teste = Imagem.nova(100, 100)  # Cria uma imagem em branco 100x100
+    imagem_teste.mostrar()
     pass
     # O código a seguir fará com que as janelas de Imagem.mostrar
     # sejam exibidas corretamente, quer estejamos executando
